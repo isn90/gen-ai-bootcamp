1d8971407af9740079ff3f7d4c888d0df2a0183a
isn90 <67857572+isn90@users.noreply.github.com>
2025-03-28 10:10:53 +0530

    Task 1: Chat completion changes
    Adding chat completion changes for Gen AI Task 1

---
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/config/OpenAIConfig.java
@@ -0,0 +1,32 @@
+package com.epam.training.gen.ai.config;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
+import com.azure.core.credential.AzureKeyCredential;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenAIConfig {
+
+    @Value("${client-openai-key}")
+    private String apiKey;
+
+    @Value("${client-openai-endpoint}")
+    private String endpoint;
+
+    @Bean
+    public OpenAIAsyncClient openAIAsyncClient() {
+        return new OpenAIClientBuilder()
+                .credential(new AzureKeyCredential(apiKey))
+                .endpoint(endpoint)
+                .buildAsyncClient();
+    }
+
+    @Bean
+    public ChatHistory chatHistory() {
+        return new ChatHistory();
+    }
+}
---
+++ tasks/gen_ai_training/src/main/java/com/epam/training/gen/ai/service/PromptService.java
@@ -0,0 +1,43 @@
+package com.epam.training.gen.ai.service;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.models.ChatCompletionsOptions;
+import com.azure.ai.openai.models.ChatRequestUserMessage;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+public class PromptService {
+
+    private static final Logger log = LoggerFactory.getLogger(PromptService.class);
+    private final OpenAIAsyncClient aiAsyncClient;
+    private final String deploymentName;
+    
+
+    public PromptService(OpenAIAsyncClient aiAsyncClient, @Value("${client-openai-deployment-name}") String deploymentName) {
+		this.aiAsyncClient = aiAsyncClient;
+		this.deploymentName = deploymentName;
+	}
+
+	public List<String> getChatCompletions(String prompt) {
+		log.info(prompt);
+		var completions = aiAsyncClient
+                .getChatCompletions(deploymentName, new ChatCompletionsOptions(List.of(new ChatRequestUserMessage(prompt))))
+                .block();
+		log.info("" + completions);
+        
+		var responses = completions.getChoices().stream()
+                .map(comp -> comp.getMessage().getContent())
+                .toList();
+        log.info(responses.toString());
+        return responses;
+    }
+}
